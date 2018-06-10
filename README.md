049afe9e972066e163cf3a016a76d8dbc0f27bc5
vbarani <vbaraniprabakar@yahoo.com>
2018-06-09 22:17:43 -0700

    final change

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ frontend-nanodegree-arcade-game
 ===============================
 
  This is a classic arcade game with a Player and Enemies(Bugs).The Player should reach the water without colliding into the enemies. The Player can move left , right ,up and down using the arrow keys. The enemies move in varying speed across the pavement. If the Player collides with the enemy, the game resets and the Player moves back to the starting position. The Player wins once he reaches the water.
+
+Instructions for playing arcade game:
+
+1.Download or clone the game using the below link:
+     https://github.com/RanjaniUS/classic-arcade-game.git
+
+2.Serve the application:
+
+     $ [sudo] python -m SimpleHTTPServer
+
+3.Open the game:
+
+     $ open "http://localhost:8000"
--- js/app.js
+++ js/app.js
@@ -1,108 +1,108 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of the instances
-     this.x = x;
-     this.y = y;
-     this.speed = speed;
-    // The image/sprite  uses a helper to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // Variables applied to each of the instances
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  // The image/sprite  uses a helper to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Updates the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
 
-    this.x += this.speed * dt;
-    //Resetting the position when enemy goes off the canvas
-    if(this.x > 505){
-       this.x = -100;
-       this.speed = 100 + Math.floor(Math.random() * 300);
+  this.x += this.speed * dt;
+  //Resetting the position when enemy goes off the canvas
+  if(this.x > 505){
+    this.x = -100;
+    this.speed = 100 + Math.floor(Math.random() * 300);
+  }
+  //Collision between player and enemies
+  if (player.x < this.x + 60 &&
+    player.x + 37 > this.x &&
+    player.y < this.y + 25 &&
+    30 + player.y > this.y) {
+      player.x = 200;
+      player.y = 380;
     }
-    //Collision between player and enemies
-    if (player.x < this.x + 60 &&
-       player.x + 37 > this.x &&
-       player.y < this.y + 25 &&
-       30 + player.y > this.y) {
-       player.x = 200;
-       player.y = 400;
-   }
-};
+  };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  };
 
-//player class
-var Player = function(x,y,speed) {
-  this.x = x;
-  this.y = y;
-  this.speed = speed;
-  this.sprite = 'images/char-boy.png';
-};
+  //player class
+  var Player = function(x,y,speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+  };
 
-//Prevent player from moving out of the canvas
-Player.prototype.update = function() {
-  if (this.y > 450) {
-        this.y = 450;
+  //Prevent player from moving out of the canvas
+  Player.prototype.update = function() {
+    if (this.y > 450) {
+      this.y = 450;
     }
 
     if (this.x > 400) {
-        this.x = 400;
+      this.x = 400;
     }
 
     if (this.x < 0) {
-        this.x = 0;
+      this.x = 0;
     }
 
     // player reaching top of canvas and winning the game
     if (this.y < 0) {
-        this.x = 200;
-        this.y = 400;
+      this.x = 200;
+      this.y = 380;
     }
-};
+  };
 
-Player.prototype.render = function() {
+  Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  };
 
- var allEnemies = [];
+  var allEnemies = [];
 
-// Position "y" where the enemies will are created
-var enemyPosition = [60, 130, 215];
-var player = new Player(200, 380, 50);
-var enemy;
+  // Position "y" where the enemies will are created
+  var enemyPosition = [60, 130, 215];
+  var player = new Player(200, 380, 50);
+  var enemy;
 
-enemyPosition.forEach(function(posY) {
+  enemyPosition.forEach(function(posY) {
     enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
     allEnemies.push(enemy);
-});
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method.
+  });
+  // This listens for key presses and sends the keys to your
+  // Player.handleInput() method.
 
-Player.prototype.handleInput = function(keyPress) {
+  Player.prototype.handleInput = function(keyPress) {
     switch (keyPress) {
-        case 'left':
-            this.x -= this.speed + 50;
-            break;
-        case 'up':
-            this.y -= this.speed + 30;
-            break;
-        case 'right':
-            this.x += this.speed + 50;
-            break;
-        case 'down':
-            this.y += this.speed + 30;
-            break;
+      case 'left':
+      this.x -= this.speed + 50;
+      break;
+      case 'up':
+      this.y -= this.speed + 30;
+      break;
+      case 'right':
+      this.x += this.speed + 50;
+      break;
+      case 'down':
+      this.y += this.speed + 30;
+      break;
     }
-};
-document.addEventListener('keyup', function(e) {
+  };
+  document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+  });
